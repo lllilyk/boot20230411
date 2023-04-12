5e283fbf69d18eef2e91e4c1c50c20dab8d2ece4
kimyumin <ym0402kkk@naver.com>
2023-04-12 17:44:14 +0900

    +

--- src/main/java/com/example/demo/controller/Controller08.java
+++ src/main/java/com/example/demo/controller/Controller08.java
@@ -36,16 +36,14 @@ public class Controller08 {
 
 	@RequestMapping("link4")
 	public void method04(@ModelAttribute("product") Dto02 dto02,
-			@ModelAttribute("member") Dto03 dto03) {
+						 @ModelAttribute("member") Dto03 dto03) {
 		// 위의 코드는 아래의 3, 4번이 생략된 것
 		// 3. add attribute
 		// 4. forward
-
 	}
 
 	@RequestMapping("link5")
-	// model attribute의 이름을 생략하면 클래스 이름을 lowerCamelCase
-	// 예) CompanyMember -> companyMember
+	// model attribute의 이름을 생략x : 내가 정한 클래스명으로 들어옴
 	public void method05(@ModelAttribute("dto02") Dto02 dto02,
 						 @ModelAttribute("dto03") Dto03 dto03) {
 		//3. add attribute
@@ -55,10 +53,10 @@ public class Controller08 {
 	}
 	
 	@RequestMapping("link6")
-	// model attribute의 이름을 생략하면 클래스 이름을 lowerCamelCase
-	// 예) CompanyMember -> companyMember
+	// model attribute의 이름을 생략 : 클래스 명이 lowerCamelCase 형식에 맞춰 들어옴
+	// 예) CompanyMember -> companyMember myName02
 	public String method06(@ModelAttribute Dto02 dto02,
-			@ModelAttribute Dto03 dto03) {
+							@ModelAttribute Dto03 dto03) {
 		//3. add attribute
 		dto02.setCompany("apple");
 		dto03.setName("steve jobs");
