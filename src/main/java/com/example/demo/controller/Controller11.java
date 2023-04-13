10286817ab671e6d445493c57db6179836a34d2f
kimyumin <ym0402kkk@naver.com>
2023-04-13 14:53:37 +0900

    +

---
+++ src/main/java/com/example/demo/controller/Controller11.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.sql.*;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub11")
+public class Controller11 {
+	
+	@RequestMapping("link1")
+	public void method1() {
+		// 1. request param
+		// 2. business logic
+
+		// SELECT CustomerName FROM Customers WHERE CustomerID = 1;
+		
+		String sql = "SELECT CustomerName FROM Customers WHERE CustomerID = 1";
+			
+		// db 연결
+		Connection connection = DriverManager.getConnection(url, name, password);
+		// Statement 객체 생성
+		Statement statement = connection.createStatement();
+		// 쿼리 실행
+		// 쿼리 실행 결과 얻고
+		ResultSet resultSet = statement.executeQuery(sql);
+		// 쿼리 실행 결과 가공
+		if(resultSet.next()) {			
+			String name = resultSet.getString("CustomerName");
+		
+		}
+		
+		//3. add attribute
+		
+		//4. forward/ redirect
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/sql/05where.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Customers; -- 현재 db의 테이블 조회
+SELECT * FROM w3schools.Customers; -- 다른 db의 테이블 조회
+
+USE w3schools; -- 사용할 db 선택하는 명령문
+USE test; 
+
+-- <= : 작거나 같다. 
